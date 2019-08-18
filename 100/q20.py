9244e93c734d5c51b49af38f6061b4a000a39104
mengzh <mengzh1618@gmail.com>
2019-08-18 22:25:00 +0800

    update q20

---
+++ 100/q20.py
@@ -0,0 +1,16 @@
+'''
+使用生成器定义一个类，该生成器可以再给定范围0和n之间迭代可被7整除的数字u
+
+
+'''
+
+def putNumebers(n):
+    i = 0
+    while i <n:
+        j = i
+        i = i+1
+        if j%7 == 0:
+            yield j
+
+for i in putNumebers(100):
+    print(i)
